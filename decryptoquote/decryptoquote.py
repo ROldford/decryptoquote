1b662ef2d5e994b64d0b1aa4865e3c26b8f40443
Ryan Oldford <ryan.oldford@gmail.com>
2019-07-16 21:09:30 -0230

    Input is now turned into word tokens with related plaintext tokens

--- decryptoquote/decryptoquote.py
+++ decryptoquote/decryptoquote.py
@@ -2,5 +2,25 @@
 
 """Main module."""
 
-def decryptQuote(crypto):
-    return crypto
+import re
+
+def stringToCapsWords(input):
+    input_caps = input.upper()
+    return re.findall(r"[\w']+|[.,!?;]", input_caps)
+
+def generateEmptyPlaintextWords(input):
+    output_words = []
+    for word in input:
+        output_word = ""
+        for char in word:
+            if char.isalpha():
+                output_word += "*"
+            else:
+                output_word += char
+        output_words.append(output_word)
+    return output_words
+
+def decryptQuote(coded_quote):
+    coded_quote_words = stringToCapsWords(coded_quote)
+    plaintext_words = generateEmptyPlaintextWords(coded_quote_words)
+    return coded_quote
--- tests/unit/test_unit.py
+++ tests/unit/test_unit.py
@@ -9,5 +9,12 @@ from decryptoquote import decryptoquote
 
 class TestUnit(object):
 
-    def test_sample(self):
-        assert True
+    def test_stringToCapsWords(self):
+        input = "Hello, I'm a string!"
+        expected = ['HELLO', ',', "I'M", 'A', 'STRING', '!']
+        assert decryptoquote.stringToCapsWords(input) == expected
+
+    def test_generateEmptyPlaintextWords(self):
+        input = ['HELLO', ',', "I'M", 'A', 'STRING', '!']
+        expected = ['*****', ',', "*'*", '*', '******', '!']
+        assert decryptoquote.generateEmptyPlaintextWords(input) == expected
