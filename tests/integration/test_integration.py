97379a8ad5c76d6b8f8e4a35c551bcd3cf7d282b
Ryan Oldford <ryan.oldford@gmail.com>
2019-07-16 21:08:15 -0230

    Integration tests now work but expect fail

--- tests/integration/test_integration.py
+++ tests/integration/test_integration.py
@@ -7,26 +7,40 @@ import pytest
 
 from decryptoquote import decryptoquote
 
+@pytest.mark.xfail
 class TestIntegration(object):
-    def test_quote_1(self):
-        coded_quote = """   KYHJ HJMHMQM FKNJ LZHJX YQQZJPZP QN. XNP ACHMKZTM
-                            QN FM HJ NFT KDZYMFTZM, MKZYEM HJ NFT SNJMSHZJSZM,
-                            LFQ MCNFQM HJ NFT KYHJM. HQ HM CHM OZXYKCNJZ QN
-                            TNFMZ Y PZYV ANTDP."""
-        plaintext = """     PAIN INSISTS UPON BEING ATTENDED TO. GOD WHISPERS
-                            TO US IN OUR PLEASURES, SPEAKS IN OUR CONSCIENCES,
-                            BUT SHOUTS IN OUR PAINS. IT IS HIS MEGAPHONE TO
-                            ROUSE A DEAF WORLD."""
-        result = decryptoquote.decryptQuote(coded_quote)
-        assert type(result).__name__ == 'str'
+    test_data = [
+        (
+            """ KYHJ HJMHMQM FKNJ LZHJX YQQZJPZP QN. XNP ACHMKZTM
+                QN FM HJ NFT KDZYMFTZM, MKZYEM HJ NFT SNJMSHZJSZM,
+                LFQ MCNFQM HJ NFT KYHJM. HQ HM CHM OZXYKCNJZ QN
+                TNFMZ Y PZYV ANTDP.""",
+            """ PAIN INSISTS UPON BEING ATTENDED TO. GOD WHISPERS
+                TO US IN OUR PLEASURES, SPEAKS IN OUR CONSCIENCES,
+                BUT SHOUTS IN OUR PAINS. IT IS HIS MEGAPHONE TO
+                ROUSE A DEAF WORLD."""
+        ),
+        (
+            """ ML INH'DX SNV JEOMST FNJXNSX XZFX'F ZMLX RXVVXD,
+                VQXS INH'DX PEFVMST INHD VMJX. INHD ZMLX PMZZ
+                RXWNJX RXVVXD RI JEOMST NVQXD ZMKXF RXVVXD.""",
+            """ IF YOU'RE NOT MAKING SOMEONE ELSE'S LIFE BETTER,
+                THEN YOU'RE WASTING YOUR TIME. YOUR LIFE WILL
+                BECOME BETTER BY MAKING OTHER LIVES BETTER."""
+        ),
+        (
+            """ JRR FSAAGFFZSR HGBHRG VGL JLM CBVGL JQG UDI MQGJVGQF. EKGN DVJIDLG
+                CKJE EKGDQ ZSESQG ABSRM UG, DMGJR DL GYGQN QGFHGAE, JLM EKGL EKGN
+                CBQW GYGQN MJN EBCJOM EKGDO MDFEJLE YDFDBL, EKJE IBJR BQ
+                HSQHBFG.""",
+            """ ALL SUCCESSFUL PEOPLE MEN AND WOMEN ARE BIG DREAMERS. THEY IMAGINE
+                WHAT THEIR FUTURE COULD BE, IDEAL IN EVERY RESPECT, AND THEN THEY
+                WORK EVERY DAY TOWARD THEIR DISTANT VISION, THAT GOAL OR
+                PURPOSE."""
+        ),
+    ]
 
-
-    def test_quote_2(self):
-        coded_quote = """   ML INH'DX SNV JEOMST FNJXNSX XZFX'F ZMLX RXVVXD,
-                            VQXS INH'DX PEFVMST INHD VMJX. INHD ZMLX PMZZ
-                            RXWNJX RXVVXD RI JEOMST NVQXD ZMKXF RXVVXD."""
-        plaintext = """     IF YOU'RE NOT MAKING SOMEONE ELSE'S LIFE BETTER,
-                            THEN YOU'RE WASTING YOUR TIME. YOUR LIFE WILL
-                            BECOME BETTER BY MAKING OTHER LIVES BETTER."""
+    @pytest.mark.parametrize("coded_quote, plaintext", test_data)
+    def test_is_correct(self, coded_quote, plaintext):
         result = decryptoquote.decryptQuote(coded_quote)
-        assert type(result).__name__ == 'str'
+        assert result == plaintext
