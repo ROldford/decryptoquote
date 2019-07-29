df01ba335180eb74013aa4ac33df72545b0b9a0b
Ryan Oldford <ryan.oldford@gmail.com>
2019-07-29 14:22:18 -0230

    Test that Puzzle can be made from existing data, not just text string

--- tests/unit/test_puzzle.py
+++ tests/unit/test_puzzle.py
@@ -11,8 +11,8 @@ test_string = "Hello, I'm a string!"
 test_coded_quote_words = ['HELLO', ',', "I'M", 'A', 'STRING', '!']
 decoded = {
     "blank": ['*****', ',', "*'*", '*', '******', '!'],
-    "in_progress": ['*****', ',', "*'*", '*', '******', '!'],
-    "finished": ['*****', ',', "*'*", '*', '******', '!']
+    "in_progress": ['**OO*', ',', "*'*", 'Z', '**I***', '!'],
+    "finished": ['SVOOL', ',', "R'N", 'Z', 'HGIRMT', '!']
 }
 coding_dict = {
     "blank":
@@ -25,19 +25,19 @@ coding_dict = {
         },
     "in_progress":
         {
-            "A": "*", "B": "*", "C": "*", "D": "*", "E": "*",
+            "A": "Z", "B": "*", "C": "*", "D": "*", "E": "*",
             "F": "*", "G": "*", "H": "*", "I": "*", "J": "*",
-            "K": "*", "L": "*", "M": "*", "N": "*", "O": "*",
-            "P": "*", "Q": "*", "R": "*", "S": "*", "T": "*",
+            "K": "*", "L": "O", "M": "*", "N": "*", "O": "*",
+            "P": "*", "Q": "*", "R": "I", "S": "*", "T": "*",
             "U": "*", "V": "*", "W": "*", "X": "*", "Y": "*", "Z": "*"
         },
     "finished":
         {
-            "A": "*", "B": "*", "C": "*", "D": "*", "E": "*",
-            "F": "*", "G": "*", "H": "*", "I": "*", "J": "*",
-            "K": "*", "L": "*", "M": "*", "N": "*", "O": "*",
-            "P": "*", "Q": "*", "R": "*", "S": "*", "T": "*",
-            "U": "*", "V": "*", "W": "*", "X": "*", "Y": "*", "Z": "*"
+            "A": "Z", "B": "Y", "C": "X", "D": "W", "E": "V",
+            "F": "U", "G": "T", "H": "S", "I": "R", "J": "Q",
+            "K": "P", "L": "O", "M": "N", "N": "M", "O": "L",
+            "P": "K", "Q": "J", "R": "I", "S": "H", "T": "G",
+            "U": "F", "V": "E", "W": "D", "X": "C", "Y": "B", "Z": "A"
         }
 }
 
@@ -69,10 +69,13 @@ def test_initCodingDict(puzzle_blank):
     expected = coding_dict["blank"]
     assert puzzle_blank.initCodingDict() == expected
 
-def test_initPuzzle(puzzle_blank):
+def test_initPuzzle(puzzle_blank, puzzle_in_progress):
     assert puzzle_blank.coded_words == test_coded_quote_words
     assert puzzle_blank.decoded_words == decoded["blank"]
     assert puzzle_blank.coding_dict == coding_dict["blank"]
+    assert puzzle_in_progress.coded_words == test_coded_quote_words
+    assert puzzle_in_progress.decoded_words == decoded["in_progress"]
+    assert puzzle_in_progress.coding_dict == coding_dict["in_progress"]
 
 def test_codingDictIsValid(puzzle_blank):
     assert True
