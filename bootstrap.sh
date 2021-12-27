62da41d8d121e7d4d108c4c4c8aaff54522b468c
Ryan Oldford <ryan.oldford@gmail.com>
2021-12-27 16:36:36 -0330

    Heroku deployment fix

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-export FLASK_APP=./api/app.py
+export FLASK_APP=./app.py
 source $(pipenv --venv)/bin/activate
 flask run -h 0.0.0.0
--- debugserver.sh
+++ debugserver.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-export FLASK_APP=./api/app.py
+export FLASK_APP=./app.py
 export FLASK_ENV=development
 source $(pipenv --venv)/bin/activate
 flask run -h 0.0.0.0
